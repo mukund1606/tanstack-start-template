511a457a8f11d0f2ebaef074fb72eccad1b10ab9
Mukund Mittal <mukund.mukulmittal@gmail.com>
2025-03-30 23:31:56 +0530

    removed migrations

--- drizzle/0001_powerful_midnight.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `tanstack_start_users` MODIFY COLUMN `username` varchar(255) NOT NULL;--> statement-breakpoint
-ALTER TABLE `tanstack_start_users` MODIFY COLUMN `two_factor_enabled` boolean NOT NULL DEFAULT false;--> statement-breakpoint
-ALTER TABLE `tanstack_start_users` ADD CONSTRAINT `tanstack_start_users_username_unique` UNIQUE(`username`);
--- drizzle/0002_orange_carnage.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `tanstack_start_users` MODIFY COLUMN `email_verified` boolean NOT NULL DEFAULT false;--> statement-breakpoint
-ALTER TABLE `tanstack_start_users` ADD `display_username` text;
